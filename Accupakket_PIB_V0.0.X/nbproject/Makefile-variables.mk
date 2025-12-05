11222952b85d3f9826956f79f3524e58eb2e0870
NielsvdB <S1215172@student.windesheim.nl>
2025-12-05 14:26:53 +0100

    Test undid

---
+++ Accupakket_PIB_V0.0.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Accupakket_PIB_V0.0.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Accupakket_PIB_V0.0.X.production.hex
